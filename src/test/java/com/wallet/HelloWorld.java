77c54cdfa61a00e3e51d595e5079e25301c0408e
Bessa <andrebessaart@gmail.com>
2021-02-15 16:54:42 -0300

    configuração inicial para os testes

---
+++ src/test/java/com/wallet/HelloWorld.java
@@ -0,0 +1,19 @@
+package com.wallet;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HelloWorld {
+	
+	@Test
+	public void testHelloWorld() {
+		assertEquals(1, 1);
+	}
+
+}
